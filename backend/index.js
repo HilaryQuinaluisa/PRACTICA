407962551b6ce100225f083e74a27e0ff6798581
HilaryQuinaluisa <hilary.quinaluisa049@ist17dejulio.edu.ec>
2025-04-22 11:00:05 -0500

    Práctica WebService - Tarea en clase [v3]

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3030;
+
+// Permitir CORS
+app.use(cors());
+
+// Endpoint ping
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong-Quinaluisa' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
